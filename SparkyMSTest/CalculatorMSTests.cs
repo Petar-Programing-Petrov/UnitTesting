3a4f4e01a5dec3213be183a01101eab2e128c86b
Petar <petar.dragov.petrov@gmail.com>
2022-10-27 16:14:48 +0200

    Initial commit

---
+++ SparkyMSTest/CalculatorMSTests.cs
@@ -0,0 +1,28 @@
+﻿using Sparky;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparkyMSTest
+{
+    [TestClass]
+    public class CalculatorMSTests
+    {
+        [TestMethod]
+        public void AddNumbers_InputOfTwoInt_GetCorrectAddition()
+        {
+            //Arrange
+            Calculator calculator = new Calculator();
+            int expected = 30; 
+            //Act
+            int actual = calculator.AddTwoNummbers(10, 20);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+
+        }
+    }
+}
---
+++ SparkyNUnitTests/CalculatorNUnitTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparky
+{
+    [TestFixture]
+    public class CalculatorNUnitTests
+    {
+        Calculator calculator = new Calculator();
+        [Test]
+        public void AddNumbers_InputOfTwoInt_GetCorrectAddition()
+        {
+            //Arrange
+           
+            int expected = 30;
+            //Act
+            int actual = calculator.AddTwoNummbers(10, 20);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void CheckNumber_IntNumber_GetOddNumber()
+        {
+            //Arange
+            
+            //Act
+            bool actual = calculator.IsOddNumber(1);
+
+            //Assert
+            Assert.IsTrue(actual);
+        }
+
+    }
+}
