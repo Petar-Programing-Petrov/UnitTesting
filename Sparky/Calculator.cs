c2161424efb2c233bd652191b2d45def8519c118
Petar <petar.dragov.petrov@gmail.com>
2022-10-28 10:52:17 +0200

    FEAT: new tipe of tests for NUnit added WIP

--- Sparky/Calculator.cs
+++ Sparky/Calculator.cs
@@ -6,6 +6,10 @@
         {
             return a + b;
         }
+        public double AddTwoDoubles(double a, double b)
+        {
+            return a + b;
+        }
         public bool IsOddNumber(int a) 
         {
             return a % 2 != 0; 
--- SparkyNUnitTests/CalculatorNUnitTests.cs
+++ SparkyNUnitTests/CalculatorNUnitTests.cs
@@ -10,6 +10,7 @@ namespace Sparky
     [TestFixture]
     public class CalculatorNUnitTests
     {
+        #region AddNumbersTests
         Calculator calculator = new Calculator();
         [Test]
         public void AddNumbers_InputOfTwoInt_GetCorrectAddition()
@@ -23,18 +24,60 @@ namespace Sparky
             //Assert
             Assert.AreEqual(expected, actual);
         }
+        [Test]
+        [TestCase(5.4, 10.5)] //15.9
+        [TestCase(5.43, 10.53)] //15.96
+        [TestCase(5.49, 10.59)] //16.08
+        public void AddNumbersDouble_InputTwoDoble_GetCorrectAddition(double a, double b)
+        {
+            //Arrange
+
+
+            //Act
+            double result = calculator.AddTwoDoubles(a, b);
+            //delta is the number of range ex. between 15.9 and 16.9 delta is 1 
+            double delta = 1;
+            //Assert
+            Assert.AreEqual(15.9,result, delta);
+        }
+        #endregion
+        #region OddNumberTests
 
         [Test]
-        public void CheckNumber_IntNumber_GetOddNumber()
+        public void IsNumberOdd_IntNumber_ReturnFalse()
         {
-            //Arange
-            
+            //Arange         
             //Act
-            bool actual = calculator.IsOddNumber(1);
+            bool actual = calculator.IsOddNumber(10);
 
             //Assert
-            Assert.IsTrue(actual);
+
+            Assert.That(actual, Is.EqualTo(false));
+ 
         }
 
+        [Test]
+        [TestCase(11)]
+        [TestCase(13)]
+        public void IsNumberOdd_IntNumber_ReturnTrue(int a)
+        {
+            //Arange
+            //Act
+            bool actual = calculator.IsOddNumber(a);
+
+            //Assert
+            Assert.That(actual, Is.EqualTo(true));
+
+        }
+
+        [Test]
+        [TestCase(10, ExpectedResult = false)]
+        [TestCase(11, ExpectedResult = true)]
+        public bool IsOddChecker_InputNumber_ReturnTrueIfOdd(int a)
+        {
+            bool result = calculator.IsOddNumber((int)a);
+            return result;
+        }
+        #endregion
     }
 }
