8cf1792735406c50aab7708031f825985270697b
Petar <petar.dragov.petrov@gmail.com>
2022-12-14 13:31:47 +0100

    FEAT: New test Objects

---
+++ Sparky/BankAccount.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparky
+{
+    public class BankAccount
+    {
+        public int Balance { get; set; }
+
+        private readonly ILogBook _logBook;
+
+        public BankAccount(ILogBook logBook)
+        {
+            _logBook = logBook;
+            Balance = 0;
+        }
+
+
+        public bool Deposit(int ammount)
+        {
+            Balance += ammount;
+            return true;
+        }
+
+        public bool Withdraw(int ammount)
+        {
+            if (ammount <= Balance)
+            {
+                Balance -= ammount;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public int GetBalance()
+        {
+            return Balance;
+        }
+    }
+}
